8867a8e0901f7f468ef2c580c52838ec6d397074
Miguel Vega <vegaleugim@gmail.com>
2022-01-03 19:39:16 -0500

    INITIAL COMMIT

---
+++ android/app/src/main/kotlin/dev/atleugim/flutter_monet/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.atleugim.flutter_monet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_monet/src/index.dart';
+
+void main() => runApp(const FlutterMonet());
